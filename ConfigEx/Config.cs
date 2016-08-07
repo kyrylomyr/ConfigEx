31ee20ecad1ee8313abddb0fb48c43d3b018ccb9
Kyrylo M <kirmir@outlook.com>
2016-08-08 01:44:53 +0300

    Implemented lazy construction of readers

--- ConfigEx/Config.cs
+++ ConfigEx/Config.cs
@@ -1,24 +1,67 @@
-﻿using ConfigEx.AssemblyProviders;
+﻿using System;
+using ConfigEx.AssemblyProviders;
+using ConfigEx.ConfigCache;
 using ConfigEx.ConfigProviders;
 using ConfigEx.ConfigReaders;
 using ConfigEx.TypeConverters;
 
 namespace ConfigEx
 {
-    public sealed class Config
+    public static class Config
     {
-        private static IConfigReader _mainConfigReader;
-        private static IConfigReader _localConfigReader;
+        private static readonly Lazy<IConfigReader> _lazyMainConfigReader =
+            new Lazy<IConfigReader>(InitMainConfigReader);
 
-        static Config()
+        private static readonly Lazy<IConfigReader> _lazyLocalConfigReader =
+            new Lazy<IConfigReader>(InitLocalConfigReader);
+
+        private static ITypeConverter _typeConverter;
+
+        private static IConfigReader MainConfigReader => _lazyMainConfigReader.Value;
+        private static IConfigReader LocalConfigReader => _lazyLocalConfigReader.Value;
+
+        public static void SetTypeConverter(ITypeConverter typeConverter)
         {
-            var converter = new TypeConverter();
+            if (typeConverter == null)
+            {
+                throw new ArgumentNullException(nameof(typeConverter), "Type Converter can not be null");
+            }
 
-            var mainProvider = new AssemblyConfigProvider(new MainAssemblyProvider());
-            var localProvider = new AssemblyConfigProvider(new LocalAssemblyProvider());
+            if (_lazyMainConfigReader.IsValueCreated || _lazyLocalConfigReader.IsValueCreated)
+            {
+                throw new ArgumentException(
+                    "Can not set Type Converter for already initialized Config Readers. Type Converter can be set before any Config read operation is done.");
+            }
 
-            _mainConfigReader = new ConfigReader(mainProvider, converter);
-            _localConfigReader = new ConfigReader(localProvider, converter);
+            TypeConverter = typeConverter;
+        }
+
+        public static ITypeConverter TypeConverter
+        {
+            private get { return _typeConverter; }
+            set { _typeConverter = value; }
+        }
+
+        private static IConfigReader InitMainConfigReader()
+        {
+            var assemblyProvider = new MainAssemblyProvider();
+            return CreateDefaultConfigReader(assemblyProvider);
+        }
+
+        private static IConfigReader InitLocalConfigReader()
+        {
+            var assemblyProvider = new LocalAssemblyProvider();
+            return CreateDefaultConfigReader(assemblyProvider);
+        }
+
+        private static IConfigReader CreateDefaultConfigReader(IAssemblyProvider assemblyProvider)
+        {
+            var configCache = new AssemblyConfigCache();
+
+            var mainProvider = new AssemblyConfigProvider(assemblyProvider, configCache);
+            var typeConverter = TypeConverter ?? new TypeConverter();
+
+            return new ConfigReader(mainProvider, typeConverter);
         }
     }
 }
