6101ddec4fcd3af95dea09df1be88a3025173888
Kyrylo M <kirmir@outlook.com>
2016-08-18 02:02:04 +0300

    Optimized ConfigReader so it saves only actual settings, not the whole Config

--- ConfigEx/ConfigReader.cs
+++ ConfigEx/ConfigReader.cs
@@ -6,43 +6,47 @@ namespace ConfigEx
 {
     public sealed class ConfigReader : IConfigReader
     {
-        private readonly Lazy<Configuration> _mainConfigurationLazy;
-        private readonly Lazy<Configuration> _localConfigurationLazy;
+        private readonly Lazy<KeyValueConfigurationCollection> _mainConfigurationLazy;
+        private readonly Lazy<KeyValueConfigurationCollection> _localConfigurationLazy;
 
-        private Configuration MainConfiguration => _mainConfigurationLazy.Value;
-        private Configuration LocalConfiguration => _localConfigurationLazy.Value;
+        private KeyValueConfigurationCollection MainConfiguration => _mainConfigurationLazy.Value;
+        private KeyValueConfigurationCollection LocalConfiguration => _localConfigurationLazy.Value;
 
         public ConfigReader(IConfigProvider mainConfigProvider, IConfigProvider localConfigProvider)
         {
             Contract.Requires<ArgumentNullException>(mainConfigProvider != null, "Main Config Provider can not be null");
             Contract.Requires<ArgumentNullException>(localConfigProvider != null, "Local Config Provider can not be null");
 
-            _mainConfigurationLazy = new Lazy<Configuration>(() => GetConfig(mainConfigProvider));
-            _localConfigurationLazy = new Lazy<Configuration>(() => GetConfig(localConfigProvider));
+            _mainConfigurationLazy = new Lazy<KeyValueConfigurationCollection>(() => GetConfig(mainConfigProvider));
+            _localConfigurationLazy = new Lazy<KeyValueConfigurationCollection>(() => GetConfig(localConfigProvider));
         }
 
         public string Get(string key)
         {
             Contract.Requires<ArgumentException>(!string.IsNullOrEmpty(key), "Key can not be null or empty");
 
-            var localSetting = LocalConfiguration.AppSettings.Settings[key];
+            var localSetting = LocalConfiguration[key];
             if (localSetting == null)
             {
                 return null;
             }
 
             // Local setting can be overridden in the main configuration.
-            var mainSetting = MainConfiguration.AppSettings.Settings[key];
+            var mainSetting = MainConfiguration[key];
 
             var value = mainSetting == null ? localSetting.Value : mainSetting.Value;
             return value ?? string.Empty;
         }
 
-        private static Configuration GetConfig(IConfigProvider configProvider)
+        private static KeyValueConfigurationCollection GetConfig(IConfigProvider configProvider)
         {
             var config = configProvider.Get();
-            Contract.Requires<ArgumentNullException>(config != null, "Config Provider returned null Configuration");
-            return config;
+            Contract.Requires<Exception>(config != null, "Config Provider returned null Configuration");
+
+            var appSettings = config.AppSettings;
+            Contract.Requires<Exception>(appSettings != null, $"Config file '{config.FilePath}' does not contain appSettings section");
+
+            return appSettings.Settings;
         }
     }
 }
