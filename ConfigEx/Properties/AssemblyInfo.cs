863e7fab29f74f7cfc4fed348e84c872bd6783e8
kirmir <kirmir@outlook.com>
2016-12-03 23:06:19 +0200

    Updated version

--- ConfigEx/Properties/AssemblyInfo.cs
+++ ConfigEx/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConfigEx")]
-[assembly: AssemblyDescription("Reads main and local Assembly configurations")]
+[assembly: AssemblyDescription("Reads main and local assembly configurations")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConfigEx")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
